6869d6c623dcc35359174a65b2389619cf383087
Mans-N <mans@neikter.se>
2024-11-26 12:00:11 +0100

    Added server for Audio service

--- audio_service/src/index.ts
+++ audio_service/src/index.ts
@@ -1,17 +1,17 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Middleware to parse JSON
 app.use(express.json());
 
 // Simple route
 app.get('/', (req: Request, res: Response) => {
-  res.send('BACKEND SERVICE IS HERE! :)');
+  res.send('AUDIO SERVICE IS HERE! :)');
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`BACKEND SERVICE Server is running on http://localhost:${PORT}`);
+  console.log(`AUDIO SERVICE Server is running on http://localhost:${PORT}`);
 });
