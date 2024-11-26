24d5011749a2254021535f95f4bd5cea8e9a9713
Mans-N <mans@neikter.se>
2024-11-26 16:47:56 +0100

    Upgraded the script for docker compose added call to audio service

--- docker/create-images.sh
+++ docker/create-images.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
-docker image rm frontend-app
-docker image rm backend-service
-docker image rm audio-service
 
-docker build -t frontend-app ../frontend_app
-docker build -t backend-service ../backend
-docker build -t audio-service ../audio_service
+if [ "$#" -lt 1 ]; then
+    docker image rm frontend-app
+    docker image rm backend-service
+    docker image rm audio-service
+
+    docker build -t frontend-app ../frontend_app
+    docker build -t backend-service ../backend
+    docker build -t audio-service ../audio_service
+
+    echo "Created images"
+else
+    case $1 in
+        frontend) 
+            docker image rm frontend-app
+            docker build -t frontend-app ../frontend_app
+            echo "Created frontend"
+        ;;
+        backend)
+            docker image rm backend-service
+            docker build -t backend-service ../backend
+            echo "Created backend"
+        ;;
+        audio)
+            docker image rm audio-service
+            docker build -t audio-service ../audio_service
+            echo "Created audio"
+        ;;
+        *)
+            echo "Invalid arguments"
+        ;;
+    esac
+fi
+
+
--- frontend_app/src/components/input.jsx
+++ frontend_app/src/components/input.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Input} from "@nextui-org/react";
-import {Button} from "@nextui-org/button";
+import { Input } from "@nextui-org/react";
+import { Button } from "@nextui-org/button";
 import { useState } from 'react';
 import { Toaster, toast } from 'sonner'
 import { gsap } from "gsap";
@@ -16,6 +16,7 @@ const InputSection = () => {
     const [data, setData] = useState("hej");
 
 
+
     useGSAP(() => {
         gsap.from(".input", { delay: 1.2, y: 50, opacity: 0 });
     }, []);
@@ -27,19 +28,29 @@ const InputSection = () => {
             const response = await axios.get('http://localhost:3001/'); // Replace with your backend API URL
             setData(response.data);
         } catch (error) {
-        console.error('Error fetching data:', error);
+            console.error('Error fetching data:', error);
         }
     };
-    
-    
+
+    const fetchAudioData = async () => {
+        try {
+            const response = await axios.get('http://localhost:3002/'); // Replace with your backend API URL
+            setData(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+
 
     function handleClickAudioService() {
         setPressed1(true);
         toast.info("Audio service!");
+        fetchAudioData();
         //call api to generate audio
         console.log(text); // send text to audio service
 
-        
+
     }
 
     function handleClickBackendService() {
@@ -56,8 +67,8 @@ const InputSection = () => {
         <>
             <div className="flex w-screen flex-row gap-16 justify-center items-center mt-40 input">
 
-                <Input type="email" label="Text" placeholder="Enter your text to be audiofied" size="lg" key="default" color="defualt"   
-                 className="w-96" value={text} onChange={(e) => setText(e.target.value)}/>
+                <Input type="email" label="Text" placeholder="Enter your text to be audiofied" size="lg" key="default" color="defualt"
+                    className="w-96" value={text} onChange={(e) => setText(e.target.value)} />
 
                 <Button color="primary" size='lg' isLoading={Btn1pressed} onPress={() => handleClickAudioService()}>
                     Get from audio service
@@ -69,12 +80,12 @@ const InputSection = () => {
             </div>
 
             <div className='border border-red-700 w-fit'>
-                <Toaster richColors position='bottom-right'/>
+                <Toaster richColors position='bottom-right' />
             </div>
             {data && (
-            <div className="text-center text-zinc-100">
-                <p>{data}</p>
-            </div>
+                <div className="text-center text-zinc-100">
+                    <p>{data}</p>
+                </div>
             )}
         </>
     );
