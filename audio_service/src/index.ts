16aa397976573960477ae26526e5f66d49a1caa8
Mans-N <mans@neikter.se>
2024-11-26 15:18:30 +0100

    Fixed cors error

--- audio_service/src/index.ts
+++ audio_service/src/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cors());
 
 // Simple route
 app.get('/', (req: Request, res: Response) => {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cors());
 
 // Simple route
 app.get('/', (req: Request, res: Response) => {
